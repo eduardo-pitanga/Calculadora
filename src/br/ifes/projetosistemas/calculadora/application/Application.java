15357dacc2e7a15f00183e6f7b1b89650f833132
eduardo-pitanga <edudupilo@gmail.com>
2024-10-08 20:01:25 -0300

    Programa original professor

--- src/br/ifes/projetosistemas/calculadora/application/Application.java
+++ src/br/ifes/projetosistemas/calculadora/application/Application.java
@@ -1,26 +1,15 @@
 package br.ifes.projetosistemas.calculadora.application;
-import java.lang.reflect.Method;
+import br.ifes.projetosistemas.calculadora.controller.ControllerCalc;
+import br.ifes.projetosistemas.calculadora.dto.RequestDTO;
+import br.ifes.projetosistemas.calculadora.dto.ResponseDTO;
+import br.ifes.projetosistemas.calculadora.view.Menu;
 
 public class Application {
     public static void main(String[] args) throws ClassNotFoundException {
-        try {
-            Class<?> Menu = Class.forName("br.ifes.projetosistemas.calculadora.view.Menu");
-            Object menu = Menu.getDeclaredConstructor().newInstance();
-            Method show = Menu.getMethod("show");
-
-            Object requestDTO = show.invoke(menu);
-
-            Class<?> ControllerCalc = Class.forName("br.ifes.projetosistemas.calculadora.controller.ControllerCalc");
-            Object controllerCalc = ControllerCalc.getDeclaredConstructor().newInstance();
-
-            Method calc = ControllerCalc.getMethod("calc", Class.forName("br.ifes.projetosistemas.calculadora.dto.RequestDTO"));
-            Object responseDTO = calc.invoke(controllerCalc, requestDTO);
-
-            Method showResult = Menu.getMethod("showResult", Class.forName("br.ifes.projetosistemas.calculadora.dto.ResponseDTO"));
-            showResult.invoke(menu, responseDTO);
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        Menu menu = new Menu();
+        RequestDTO requestDTO = menu.show();
+        ControllerCalc controllerCalc = new ControllerCalc();
+        ResponseDTO responseDTO = controllerCalc.calc(requestDTO);
+        menu.showResult(responseDTO);
     }
 }
